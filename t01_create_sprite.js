16548b3752590ef8ba0717263c9c71f8b3b56a89
23228cb-colab <23228cb@hvhs.school.nz>
2026-02-09 14:05:47 +1300

    New sprite's

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -13,6 +13,7 @@ function setup() {
 	Rect = new Sprite(windowWidth/2, windowHeight/2, 20, 10 );
 	Rect.color = "Red";
 	Circ = new Sprite(windowWidth/5, windowHeight/2, 40);
+	Circ.color = "Blue"
 }
 	
 /*******************************************************/
