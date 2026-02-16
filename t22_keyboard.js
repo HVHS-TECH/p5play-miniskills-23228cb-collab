4215fd0978ce4c8a6b502ae81e77d0203fa47e9e
23228cb-colab <23228cb@hvhs.school.nz>
2026-02-17 12:28:25 +1300

    commit+push

--- t22_keyboard.js
+++ t22_keyboard.js
@@ -11,9 +11,19 @@ function setup() {
 	console.log("setup: ");
 
 	cnv = new Canvas( windowWidth, windowHeight-40);
+	world.gravity.y = 1000;
 
 	Rect = new Sprite(windowWidth/2, windowHeight/2, windowWidth/5, windowHeight/10, 'd' );
 	Rect.color = "Red";
+
+	wallLH  = new Sprite(0, windowHeight/2, 8, windowHeight, 'k');
+	wallLH.color = "Black";
+	wallRH  = new Sprite(windowWidth, windowHeight/2, 8, windowHeight, 'k');
+	wallRH.color = "Black";
+	wallTop = new Sprite(windowWidth/2, 0, windowWidth, 8, 'k');
+	wallTop.color = "Black";
+	wallBot = new Sprite(windowWidth/2, windowHeight, windowWidth, 8, 'k');
+	wallBot.color = "Black";
 }
 	
 /*******************************************************/
@@ -21,17 +31,13 @@ function setup() {
 /*******************************************************/
 function draw() {
 	background("white");
-	//Rect left/right
-	if (kb.pressing('left')) {
-
-    
-		Rect.vel.x = -2
+//Rect left/right
+if (kb.pressing('left')) {
+	Rect.vel.x = -20
 }
 
 else if (kb.pressing ('right')) {
-
-   	Rect.vel.x = 2   
-
+   	Rect.vel.x = 20  
 };
 
 if (kb.released('left')){
@@ -43,15 +49,11 @@ else if (kb.released('right')){
 
 //Rect Up/down
 if (kb.pressing('down')) {
-
-    
-	Rect.vel.y = 2
+    	Rect.vel.y = 20
 }
 
 else if (kb.pressing ('up')) {
-
-   	Rect.vel.y = -2   
-
+   	Rect.vel.y = -20
 };
 
 if (kb.released('down')){
@@ -61,22 +63,18 @@ else if (kb.released('up')){
 	Rect.vel.y = 0
 };
 //Rect Rotation
-if (kb.pressing('a')) {
-
-    
-	Rect.rotationSpeed = 2
+if (kb.pressing('e')) {
+	Rect.rotationSpeed = 20
 }
 
-else if (kb.pressing ('d')) {
-
-   	Rect.rotationSpeed = -2   
-
+else if (kb.pressing ('q')) {
+   	Rect.rotationSpeed = -20   
 };
 
-if (kb.released('d')){
+if (kb.released('q')){
 	Rect.rotationSpeed = 0
 }
-else if (kb.released('a')){
+else if (kb.released('e')){
 	Rect.rotationSpeed = 0
 };
 	
